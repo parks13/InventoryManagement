223e56bba67e905c5e95501a8067b8c8b0d90780
Jason Sungmin Park <parks13@spu.edu>
2019-07-30 19:03:23 -0600

    Reverted

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 import sys, csv
 from PyQt5 import QtCore, uic, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QLineEdit, QMessageBox, QPushButton, QTextEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QLineEdit, QMessageBox, QPushButton, QTextEdit, \
+    QVBoxLayout, QWidget, QFileDialog, QTableWidget, QTableWidgetItem
 from PyQt5.QtGui import QTextCharFormat, QTextFormat, QTextObjectInterface
 from PyQt5.QtCore import QFile, QIODevice, QObject, QSizeF
 
@@ -25,29 +26,37 @@ class InvGUI(UIClass, QtBaseClass):
 
     # Opens a single .csv file
     def openFile(self):
-        options = QtWidgets.QFileDialog.Options()
-        options |= QtWidgets.QFileDialog.DontUseNativeDialog
-        filePath = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '*.csv', options=options)
-        file = QFile(filePath)
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        filePath = QFileDialog.getOpenFileName(self, 'Open File', '', 'CSV(*.csv)', options=options)
 
         # If file is opened successfully, parse the data into the QTableView
-        if file:
+        if filePath[0] != '':
+            try:
+                # parse the csv file
+                with open(filePath[0], newline='') as csvFile:
+                    self.tableWidget.setRowCount(0)
+                    self.tableWidget.setColumnCount(3)
+                    file = csv.reader(csvFile, delimiter=',', quotechar='|')
 
-            # parse the csv file
-            with open(file, "w") as fileInput:
-                for row in csv.reader(fileInput):
-                    items = [
-                        QtGui.QStandardItem(field)
-                        for field in row
-                    ]
-                    self.tableView.appendRow(items)
+                    for rowData in file:
+                        row = self.tableWidget.rowCount()
+                        self.tableWidget.insertRow(row)
 
-            # Move to next page
-            self.stackedWidget.setCurrentIndex(1)
+                        for column, colData in enumerate(rowData):
+                            item = QTableWidgetItem(colData)
+                            self.tableWidget.setItem(row, column, item)
 
-        # If file is not open, display message and stay on same page
-        else:
-            QMessageBox.warning(self, "ERROR", '','','')
+                # Move to next page
+                self.stackedWidget.setCurrentIndex(1)
+
+            except:
+                # If file is not open, display message and stay on same page
+                warningMsg = QMessageBox()
+                warningMsg.setIcon(QMessageBox.Warning)
+                warningMsg.setWindowTitle('ERROR')
+                warningMsg.setText('Error loading file')
+                warningMsg.exec_()
 
 
 app = QtWidgets.QApplication(sys.argv)
